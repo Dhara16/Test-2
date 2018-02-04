6db6dc93d358f3620321d1bd178faf9878e4d654
Dhara16 <dhara0128@gmail.com>
2018-02-04 13:05:55 +0000

    First commit added

---
+++ src/Day_1.java
@@ -0,0 +1,9 @@
+    public static void main (String[]args){
+    String greeting ="Hi Hello How are you";
+    Systm.out.print(greeting);
+        System.out.print("Hello");
+        System.out.println("World");
+        System.out.print ("Sky");
+        int x; //int y;
+    }
+}
