f5e4cbbddb11a679c8211e01214b0421ac1b96f0
Dhara16 <dhara0128@gmail.com>
2018-02-04 21:04:51 +0000

    Added few more prints

--- src/Day_1.java
+++ src/Day_1.java
@@ -1,7 +1,9 @@
     public static void main (String[]args){
     String greeting ="Hi Hello How are you";
-    Systm.out.print(greeting);
+    System.out.print(greeting);
         System.out.print("Hello");
+        System.out.print("1234");
+        System.out.println("ABCD");
         System.out.println("World");
         System.out.print ("Sky");
         int x; //int y;
